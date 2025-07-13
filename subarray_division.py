37f2009c4b9c2908108165d16f8f7010c7882676
Didar <didar-145@hotmail.com>
2025-07-13 23:52:51 +0300

    subarray_d

---
+++ subarray_division.py
@@ -0,0 +1,12 @@
+def birthday(s, d, m):
+    count = 0
+    for i in range(len(s) - m + 1):
+        if sum(s[i:i + m]) == d:
+            count += 1
+    return count
+
+
+n = int(input())
+s = list(map(int, input().strip().split()))
+d, m = map(int, input().strip().split())
+print(birthday(s, d, m))
